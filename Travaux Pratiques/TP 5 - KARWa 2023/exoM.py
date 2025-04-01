c3d27dc5a62d8306003d831d130f7c27a752caa5
210668 <210668@umons.ac.be>
2025-04-01 16:44:06 +0200

    TP 5

--- Travaux Pratiques/TP 5 - KARWa 2023/exoM.py
+++ Travaux Pratiques/TP 5 - KARWa 2023/exoM.py
@@ -3,7 +3,4 @@ import math
 d = int(input())
 alpha = int(input())
 
-alpha_rad = math.radians(alpha)
-distance = 2 * d * math.tan(alpha_rad)
-
-print(distance)
+print(2 * d * math.tan(math.radians(alpha)))
